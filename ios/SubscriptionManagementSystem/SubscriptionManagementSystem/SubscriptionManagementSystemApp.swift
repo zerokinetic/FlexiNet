8cfe3f6afe1a9d588df83e141929a0ef7b669a2e
ICHIGO606 <shivamdubey177@gmail.com>
2025-09-13 09:46:37 +0530

    initial commit

---
+++ ios/SubscriptionManagementSystem/SubscriptionManagementSystem/SubscriptionManagementSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionManagementSystemApp.swift
+//  SubscriptionManagementSystem
+//
+//  Created by Shivam Dubey on 13/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionManagementSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
